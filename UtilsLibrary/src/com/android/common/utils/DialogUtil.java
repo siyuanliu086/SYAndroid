068c22803d48028c055e1dff156f38f9cd6366d0
siyuanliu086 <syliu.11@163.com>
2016-12-01 17:14:05 +0800

    -- refactor dialogutil, with nice face and better performance

--- UtilsLibrary/src/com/android/common/utils/DialogUtil.java
+++ UtilsLibrary/src/com/android/common/utils/DialogUtil.java
@@ -1,9 +1,7 @@
 package com.android.common.utils;
 
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.DialogInterface.OnDismissListener;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,7 +21,17 @@ import com.common.android.utilslibrary.R;
  */
 public class DialogUtil {
 	private Context mContext;
+	private View view;
 	private AlertDialog mAlertDialog;
+	
+	public DialogUtil(Context context) {
+		this.mContext = context;
+		AlertDialog.Builder builder = new AlertDialog.Builder(mContext, AlertDialog.THEME_HOLO_LIGHT);
+		// 设置布局
+		view = LayoutInflater.from(context).inflate(R.layout.utilslib_layout_dialog, null);
+		builder.setView(view);
+		mAlertDialog = builder.create();
+	}
 	
 	/**
 	 * 提示对话框，点击确定后消失
@@ -32,20 +40,19 @@ public class DialogUtil {
 	 * @param message
 	 * @param positiveListener
 	 */
-	public DialogUtil(Context context, String title, String message) {
-		this.mContext = context;
-		AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-		// 设置date布局
-		builder.setTitle(title);
-		builder.setMessage(message);
-		builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+	public void setDialog(String title, String message) {
+		TextView titleTv = (TextView) view.findViewById(R.id.dialog_title_tv);
+		TextView contentTv = (TextView) view.findViewById(R.id.dialog_content_tv);
+		TextView okTv = (TextView) view.findViewById(R.id.dialog_ok_tv);
+		titleTv.setText(title);
+		contentTv.setText(message);
+		okTv.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
-			public void onClick(DialogInterface arg0, int arg1) {
+			public void onClick(View v) {
 				mAlertDialog.dismiss();
 			}
 		});
-		mAlertDialog = builder.create();
 	}
 	
 	/**
@@ -55,15 +62,13 @@ public class DialogUtil {
 	 * @param message
 	 * @param positiveListener
 	 */
-	public DialogUtil(Context context, String title, String message, 
-			DialogInterface.OnClickListener positiveClickListener) {
-		this.mContext = context;
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		// 设置date布局
-		builder.setTitle(title);
-		builder.setMessage(message);
-		builder.setPositiveButton(android.R.string.ok, positiveClickListener);
-		mAlertDialog = builder.create();
+	public void setDialog(String title, String message, View.OnClickListener onClickListener) {
+		TextView titleTv = (TextView) view.findViewById(R.id.dialog_title_tv);
+		TextView contentTv = (TextView) view.findViewById(R.id.dialog_content_tv);
+		TextView okTv = (TextView) view.findViewById(R.id.dialog_ok_tv);
+		titleTv.setText(title);
+		contentTv.setText(message);
+		okTv.setOnClickListener(onClickListener);
 	}
 	
 	/**
@@ -73,17 +78,18 @@ public class DialogUtil {
 	 * @param message
 	 * @param positiveListener
 	 */
-	public DialogUtil(Context context, String title, String message, 
-			DialogInterface.OnClickListener positiveClickListener,
-			DialogInterface.OnClickListener negativeClickListener) {
-		this.mContext = context;
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		// 设置date布局
-		builder.setTitle(title);
-		builder.setMessage(message);
-		builder.setPositiveButton(android.R.string.ok, positiveClickListener);
-		builder.setNegativeButton(android.R.string.cancel, negativeClickListener);
-		mAlertDialog = builder.create();
+	public void setDialog(String title, String message, View.OnClickListener onOkListener, 
+			View.OnClickListener onCancelListener) {
+		// 设置布局
+		TextView titleTv = (TextView) view.findViewById(R.id.dialog_title_tv);
+		TextView contentTv = (TextView) view.findViewById(R.id.dialog_content_tv);
+		TextView okTv = (TextView) view.findViewById(R.id.dialog_ok_tv);
+		TextView cancelTv = (TextView) view.findViewById(R.id.dialog_cancel_tv);
+		view.findViewById(R.id.dialog_cancel_layout).setVisibility(View.VISIBLE);
+		titleTv.setText(title);
+		contentTv.setText(message);
+		okTv.setOnClickListener(onOkListener);
+		cancelTv.setOnClickListener(onCancelListener);
 	}
 	
 	/**
@@ -93,19 +99,22 @@ public class DialogUtil {
 	 * @param message
 	 * @param positiveListener
 	 */
-	public DialogUtil(Context context, String title, String message, 
+	public void setDialog(String title, String message, 
 			String positiveButtonText,
 			String negativeButtonText,
-			DialogInterface.OnClickListener positiveClickListener,
-			DialogInterface.OnClickListener negativeClickListener) {
-		this.mContext = context;
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		// 设置date布局
-		builder.setTitle(title);
-		builder.setMessage(message);
-		builder.setPositiveButton(positiveButtonText, positiveClickListener);
-		builder.setNegativeButton(negativeButtonText, negativeClickListener);
-		mAlertDialog = builder.create();
+			View.OnClickListener onOkListener,
+			View.OnClickListener onCancelListener) {
+		TextView titleTv = (TextView) view.findViewById(R.id.dialog_title_tv);
+		TextView contentTv = (TextView) view.findViewById(R.id.dialog_content_tv);
+		TextView okTv = (TextView) view.findViewById(R.id.dialog_ok_tv);
+		TextView cancelTv = (TextView) view.findViewById(R.id.dialog_cancel_tv);
+		view.findViewById(R.id.dialog_cancel_layout).setVisibility(View.VISIBLE);
+		titleTv.setText(title);
+		contentTv.setText(message);
+		okTv.setText(positiveButtonText);
+		cancelTv.setText(negativeButtonText);
+		okTv.setOnClickListener(onOkListener);
+		cancelTv.setOnClickListener(onCancelListener);
 	}
 	
 	/**
@@ -115,20 +124,13 @@ public class DialogUtil {
 	 * @param message
 	 * @param view
 	 */
-	@SuppressLint("NewApi")
-	public DialogUtil(Context context, String title, String message,OnClickListener onClickListener,OnDismissListener onDismissListener) {
-		this.mContext = context;
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		// 设置date布局
-		View view = LayoutInflater.from(context).inflate(R.layout.utilslib_layout_dialog, null);
+	public void setDialog(String title, String message, OnClickListener onClickListener,OnDismissListener onDismissListener) {
 		TextView titleTv = (TextView) view.findViewById(R.id.dialog_title_tv);
 		TextView contentTv = (TextView) view.findViewById(R.id.dialog_content_tv);
 		titleTv.setText(title);
 		contentTv.setText(message);
-		builder.setView(view);
 		view.setOnClickListener(onClickListener);
-		builder.setOnDismissListener(onDismissListener);
-		mAlertDialog = builder.create();
+		mAlertDialog.setOnDismissListener(onDismissListener);
 	}
 	
 	public void showDiaglog() {
