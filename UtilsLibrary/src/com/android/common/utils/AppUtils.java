11475d0dd4776f097ed49543ea191d3e5ffc3345
siyuanliu086 <syliu.11@163.com>
2016-12-01 16:09:21 +0800

    -- move DensityUtils to AppUtils

--- UtilsLibrary/src/com/android/common/utils/AppUtils.java
+++ UtilsLibrary/src/com/android/common/utils/AppUtils.java
@@ -377,6 +377,36 @@ public class AppUtils {
         int availableProcessors = 2 * Runtime.getRuntime().availableProcessors() + 1;
         return availableProcessors > max ? max : availableProcessors;
     }
+    
+    /** 
+     * 根据手机的分辨率dp 转成(像素)
+     */  
+    public static int dip2px(float density, float dpValue) {  
+        return (int) (dpValue * density + 0.5f);  
+    }  
+	
+    /** 
+     * 根据手机的分辨率dp 转成(像素)
+     */  
+    public static int dip2px(Context context, float dpValue) {  
+        final float scale = context.getResources().getDisplayMetrics().density;  
+        return (int) (dpValue * scale + 0.5f);  
+    }  
+  
+    /** 
+     *根据手机的分辨率px(像素) 的单转成dp 
+     */  
+    public static int px2dip(float density, float pxValue) {  
+        return (int) (pxValue / density + 0.5f);  
+    } 
+    
+    /** 
+     *根据手机的分辨率px(像素) 的单转成dp 
+     */  
+    public static int px2dip(Context context, float pxValue) {  
+        final float scale = context.getResources().getDisplayMetrics().density;  
+        return (int) (pxValue / scale + 0.5f);  
+    }  
 
 		
 	/**
